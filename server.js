9e6107056cfb855bea682c39e7a0436fb72ba90e
Eugene Grinberg <eugrin@gmail.com>
2016-12-29 14:56:27 -0500

    Add DELETE todos

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.get('/todos/:id', function (req, res) {
 });
 
 app.post('/todos', function(req, res){
-   var body = req.body;
-   body = _.pick(body, 'description', 'completed');
+   var body = _.pick(req.body, 'description', 'completed');
+
    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
      return res.status(400).send();
    }
@@ -42,6 +42,19 @@ app.post('/todos', function(req, res){
    todos.push(body);
 
    res.json(body);
+});
+
+app.delete('/todos/:id', function(req, res){
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  if(matchedTodo){
+    todos = _.without(todos, matchedTodo);
+    res.json(matchedTodo);
+  }
+  else {
+    res.status(404).json({"error": "no id found"});
+  }
 });
 
 app.listen(PORT, function() {
